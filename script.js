c78ee3449606de5741b26a69367fba450091d9da
Vinicius Marques Moreira <vinicius.moreira94@outlook.com>
2023-02-01 10:11:34 -0300

    update

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function toggleMode() {
   const html = document.documentElement
+  html.classList.toggle("light")
 
-  //if(html.classList.contains('light')) {
-   // html.classList.remove('light')
-  //} else {
-   // html.classList.add('light')
- // }
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
 
- html.classList.toggle('light')
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver a light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // se tiver sem a light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
 }
